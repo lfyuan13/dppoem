4d6e197c9c1899b229e84bdef0c97d53308deb94
lfyuan <lfyuan@192.168.0.102>
2016-06-21 01:55:56 +0800

    dp tree is Done

--- yicun.py
+++ yicun.py
@@ -5,6 +5,9 @@ import re
 import urllib2
 
 
+child_relation = {"SBV", "VOB", "ATT", "ADV"}
+brother_relation = {"COO"}
+ignored_pos = {"r", "u"}
 
 def analyze(text, pattern='dp'):
 	url_get_base = "http://api.ltp-cloud.com/analysis/?"
@@ -28,7 +31,7 @@ def filter2(text, word):
 
 
 class Word(object):
-	def __init__(self, pos=None, word=None, is_root=False):
+	def __init__(self, pos="", word="", is_root=False, index=0):
 		"""
 			构建本节点的：left or right 关系
 			新节点的：pre关系
@@ -39,9 +42,10 @@ class Word(object):
 			self.word = ""
 		self.word = word
 		self.pos = pos
-		self.right = None
-		self.left = None
-		self.pre = None
+		self.index = index
+		self.right = []
+		self.left = []
+		self.pre = []
 		
 
 	def add(self, relation=None, word=None):
@@ -50,15 +54,15 @@ class Word(object):
 		"""
 		
 		if relation in ['ATT', 'SBV', 'ADV']:
-			self.left = word
+			self.left.append(word)
 		elif relation in ['VOB']:
-			self.right = word
+			self.right.append(word)
 		else:
-			self.left = word
+			self.left.append(word)
 		word.setPre(self)
 
 	def setPre(self, word):
-		self.pre = word
+		self.pre.append(word)
 
 	def getLeft(self):
 		return self.left
@@ -67,29 +71,71 @@ class Word(object):
 		return self.right
 
 	def getWord(self):
-		return self.word
+		return "".join([self.word, self.pos, str(self.index)])
+
+	def getPos(self):
+		return self.pos
 
 
 def print_tree(node):
 	if node is None:
 		return
 	if node.getLeft() is not None:
-		print_tree(node.getLeft())
-	print node.getWord()
+		for left in node.getLeft():
+			print_tree(left)
+	if node.getPos() not in ignored_pos:
+		print node.getWord()
 	if node.getRight() is not None:
-		print_tree(node.getRight())
+		for right in node.getRight():
+			print_tree(right)
 
 
+def pop_item(li, kw, idx):
+	"""
+		从列表里获取满足条件的元素
+	"""
+	res = []
+	flag = True
+	while flag:
+		flag = False
+		for i, item in enumerate(li):
+			if item[idx] == kw:
+				res.append(li.pop(i)) 
+				flag = True
+				break
+	return res
 
-def generate_tree(dp=None, pos=None):
 
-	root = Word( is_root=True )
-	left = Word( pos='n', word=u'你好')
-	root.add(relation='HED', word=left)
-	right = Word( pos='n', word=u'厉害')
-	root.add(relation="VOB", word=right)
-	print_tree(root)
-	# print_tree(left)
+def add_node(node, word, dp_list, pos_map, relat, dictionary):
+	"""
+		这一个函数应该是一个递归
+		word is string
+		dp_list: dp list
+		pos_map: pos map
+	"""
+
+	node_tmp = Word(pos=pos_map[word], word=word, index=dictionary[word])
+	node.add(relation=relat, word=node_tmp)
+
+	res = pop_item(dp_list, word, 1)
+	if res is None or len(res) ==0:
+		pass
+	else:
+		for item in res:
+			if item[2] in child_relation:
+				add_node(node_tmp, item[0], dp_list, pos_map, item[2], dictionary)
+			elif item[2] in brother_relation:
+				add_node(node, item[0], dp_list, pos_map, item[2], dictionary)
+
+
+def generate_tree(dp_list=None, pos_map=None, dictionary=None):
+	root_tuple = pop_item(dp_list, -1, 1)[0]
+	root_node = Word(word="", is_root=True, index=-1)
+
+	add_node(root_node, root_tuple[0], dp_list, pos_map, root_tuple[2], dictionary)
+
+	print_tree(root_node)
+
 
 
 
@@ -111,21 +157,23 @@ def generate1(sent):
 
 		dp_list2.append((word1, word2, w3))
 
-	print dp_list2
+	print "dp:", dp_list2
 	pos = analyze(sent, 'pos')
 	pos_list = [p for p in pos.split(" ")]
-	pos_list2 = []
+	pos_map = {}
 	root = Word()
 	for item in pos_list:
 		w1, p1 = item.split("_")
-
+		pos_map[w1] = p1
+	print "pos:", pos_map
+	generate_tree(dp_list2, pos_map, dictionary)
 		# print w1, p1
 	#print pos
 
 # generate1("那花白里透红，花瓣润滑透明，像一颗颗价值不菲的水晶。")
 # generate1("梅花的色，艳丽而不妖")
 
-generate_tree()
+generate1("那花白里透红，花瓣润滑透明，像一颗颗价值不菲的水晶。")
 
 # kw = u'梅花'
 # url = "http://www.gkstk.com/article/1383483811.html"
